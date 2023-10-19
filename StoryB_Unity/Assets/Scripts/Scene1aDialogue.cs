e9d5c00388a9c080f320755d0891b79654447cd4
HunterFelt <htfelt@massart.edu>
2023-10-19 17:45:59 -0400

    Added Scenes to Build

---
+++ StoryB_Unity/Assets/Scripts/Scene1aDialogue.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene1aDialogue : MonoBehaviour {
+        public int primeInt = 1;         // This integer drives game progress!
+        public Text Char1name;
+        public Text Char1speech;
+        public Text Char2name;
+        public Text Char2speech;
+       //public Text Char3name;
+       //public Text Char3speech;
+        public GameObject DialogueDisplay;
+        public GameObject ArtChar1a;
+        public GameObject ArtBG1;
+        public GameObject Choice1a;
+        public GameObject Choice1b;
+        public GameObject NextScene1Button;
+        public GameObject NextScene2Button;
+        public GameObject NextScene3Button;
+        public GameObject NextScene4Button;
+        public GameObject nextButton;
+       //public AudioSource audioSource;
+        private bool allowSpace = true;
+
+// initial visibility settings. Any new images or buttons need to also be SetActive(false);
+void Start(){  
+        DialogueDisplay.SetActive(false);
+        ArtChar1a.SetActive(true);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        NextScene3Button.SetActive(false);
+        NextScene4Button.SetActive(false);
+        nextButton.SetActive(true);
+   }
+
+void Update(){         // use spacebar as Next button
+        if (allowSpace == true){
+                if (Input.GetKeyDown("space")){
+                       Next();
+                }
+        }
+   }
+
+//Story Units! The main story function. Players hit [NEXT] to progress to the next primeInt:
+public void Next(){
+        primeInt = primeInt + 1;
+        if (primeInt == 1){
+                // AudioSource.Play();
+        }
+        else if (primeInt == 2){
+                DialogueDisplay.SetActive(true);
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = "Well that planet was interesting, where to next?";
+                Char2name.text = "";
+                Char2speech.text = "";
+				                // Turn off "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+               // Choice1a.SetActive(true); // function Choice1aFunct()
+//Choice1b.SetActive(true); // function Choice1bFunct()
+        NextScene1Button.SetActive(true);
+		NextScene2Button.SetActive(true);
+        NextScene3Button.SetActive(true);
+		NextScene4Button.SetActive(true);
+        }
+           
+      //Please do NOT delete this final bracket that ends the Next() function:
+     }
+
+// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
+        public void Choice1aFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "I don't know what you're talking about!";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 19;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+        public void Choice1bFunct(){
+                Char1name.text = "YOU";
+                Char1speech.text = "Sure, anything you want... just lay off the club.";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 29;
+                Choice1a.SetActive(false);
+                Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+
+        public void SceneChange1(){
+               SceneManager.LoadScene("Scene_2b");
+        }
+        public void SceneChange2(){
+                SceneManager.LoadScene("Scene_r2");
+        }
+        public void SceneChange3(){
+               SceneManager.LoadScene("Scene_2d");
+        }
+        public void SceneChange4(){
+                SceneManager.LoadScene("Scene_h2");
+        }
+}
