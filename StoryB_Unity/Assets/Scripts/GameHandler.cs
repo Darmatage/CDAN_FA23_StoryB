ce5dcaac29bc613e0555f8d03773138ba027cb07
Darmatage <JasonWiserArt@gmail.com>
2023-10-19 18:54:28 -0400

    Added Pause Menu to GameHandler script slot in all scenes (i hope -- playtest!)

--- StoryB_Unity/Assets/Scripts/GameHandler.cs
+++ StoryB_Unity/Assets/Scripts/GameHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour{
 
@@ -13,32 +14,66 @@ public class GameHandler : MonoBehaviour{
 		
         // public GameObject textGameObject;
 
-        // void Start () { UpdateScore (); }
+        public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
 
-        void Update(){         //delete this quit functionality when a Pause Menu is added!
-                if (Input.GetKey("escape")){
-                        Application.Quit();
+        void Awake(){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
                 }
+        }
 
+        void Start(){
+                pauseMenuUI.SetActive(false);
+                GameisPaused = false;
+        }
+
+        void Update(){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){ Resume(); }
+                        else{ Pause(); }
+                }
                 // Stat tester:
                 //if (Input.GetKey("p")){
                 //       Debug.Log("Player Stat = " + playerStat1);
                 //}
         }
 
-        // void UpdateScore () {
-        //        Text scoreTemp = textGameObject.GetComponent<Text>();
-        //        scoreTemp.text = "Score: " + score; }
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
 
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        } 
+		
         public void StartGame(){
+			Time.timeScale = 1f;
                 SceneManager.LoadScene("Scene_1");
         }
 
         public void OpenCredits(){
+			Time.timeScale = 1f;
                 SceneManager.LoadScene("Credits");
         }
 
         public void RestartGame(){
+			Time.timeScale = 1f;
                 SceneManager.LoadScene("MainMenu");
         }
 
