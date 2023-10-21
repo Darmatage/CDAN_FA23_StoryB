df75c746e1acffdc609ab60da084ee2eba6289d5
emmacmurray <emmacmurrayy@gmail.com>
2023-10-20 22:10:02 -0400

    scene 5a  changes

--- StoryB_Unity/Assets/Scripts/Scene_5a_dialogue.cs
+++ StoryB_Unity/Assets/Scripts/Scene_5a_dialogue.cs
@@ -18,15 +18,18 @@ public class Scene_5a_dialogue : MonoBehaviour {
         public GameObject ArtChar1b; // smeg confused
         public GameObject ArtChar1c; //smeg relieved
         public GameObject ArtChar1d; // smeg scared
-        public GameObject ArtBG1;
-		public GameObject ArtBG2;
-		public GameObject ArtBG3;
-		public GameObject ArtBG4;
-		public GameObject ArtBG5;
+        public GameObject ArtBG1; //usa
+		public GameObject ArtBG2; // area 51
+		public GameObject ArtBG3; //black
+		public GameObject ArtBG4; //pyramid giza
+		public GameObject ArtBG5; //pyramid int
         public GameObject Choice1a;
         public GameObject Choice1b;
+		public GameObject Choice2a;
+        public GameObject Choice2b;
         public GameObject NextScene1Button;
         public GameObject NextScene2Button;
+		public GameObject NextScene3Button;
         public GameObject nextButton;
        //public AudioSource audioSource;
         private bool allowSpace = true;
@@ -42,8 +45,11 @@ void Start(){
 		ArtBG5.SetActive(false);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
+		Choice2a.SetActive(false);
+        Choice2b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
+		NextScene3Button.SetActive(false);
         nextButton.SetActive(true);
    }
 
@@ -74,7 +80,6 @@ public void Next(){
                 Char1speech.text = "But… while I was landing, I saw an abundance of h2o, and there appears to be none where I landed…";
                 Char2name.text = "";
                 Char2speech.text = "";
-                //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 4){
                 Char1name.text = "Cadet Smeg";
@@ -87,49 +92,22 @@ public void Next(){
                 Choice1a.SetActive(true); // function Choice1aFunct()
                 Choice1b.SetActive(true); // function Choice1bFunct()
         }
-       else if (primeInt == 5){
-                Char1name.text = "YOU";
-                Char1speech.text = "Hit me? Why?";
-                Char2name.text = "";
-                Char2speech.text = "";
-        }
-       else if (primeInt == 6){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "I am searching for a fugitive. Ragu Fahn.";
-        }
-       else if (primeInt ==7){
-                Char1name.text = "YOU";
-                Char1speech.text = "Why do you think I know anything?";
-                Char2name.text = "";
-                Char2speech.text = "";
-        }
-       else if (primeInt == 8){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Do not play the stupid. You will take me to him.";
-
-        }
+		
 
        // after choice 1a
        else if (primeInt == 20){
-                //gameHandler.AddPlayerStat(1);
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "???";
                 Char2speech.text = "Target has been spotted.";
         }
 	   else if (primeInt == 21){
-                //gameHandler.AddPlayerStat(1);
                 Char1name.text = "Cadet Smeg";
                 Char1speech.text = "Huh? Who was that?";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
 	    else if (primeInt == 22){
-                //gameHandler.AddPlayerStat(1);
                 Char1name.text = "Cadet Smeg";
                 Char1speech.text = "Hello? Is this your planet? Can you help me?";
                 Char2name.text = "";
@@ -225,18 +203,25 @@ public void Next(){
                 Char2name.text = "???";
                 Char2speech.text = "Welcome to Area 51. Prepare to spend the rest of your life here.";
         }
-		else if (primeInt == 36){
+		else if (primeInt == 37){
                 Char1name.text = "Cadet Smeg";
                 Char1speech.text = "What are you going to do to me?";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
-		else if (primeInt == 37){
+		else if (primeInt == 38){
                 Char1name.text = "";
                 Char1speech.text = "";
                 Char2name.text = "???";
                 Char2speech.text = "That information is classified.";
+				//Go to next scene
+				nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene1Button.SetActive(true);
         }
+		
+		
+		
        // after choice 1b
        else if (primeInt == 49){
 		    ArtBG1.SetActive(false);
@@ -266,13 +251,101 @@ public void Next(){
                 Char2name.text = "";
                 Char2speech.text = "";
         }
+		    else if (primeInt == 53 ){
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = " What’s that over there? Is that...";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+			else if (primeInt == 54 ){
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = "Wow! This is ancient technology! Communication devices, resources… and weapons.";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+			else if (primeInt == 55 ){
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = "Maybe.. Just maybe.. We could stay here instead!";
+                Char2name.text = "";
+                Char2speech.text = "";
+
+				// Turn off "Next" button, turn on "Choice" buttons
+                nextButton.SetActive(false);
+                allowSpace = false;
+                Choice2a.SetActive(true); // function Choice2aFunct()
+                Choice2b.SetActive(true); // function Choice3bFunct()	
+        }
+		
+		
+		//after choice 2a
+       else if (primeInt == 60){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 61){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt ==62){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 63){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				//Go to next scene
+				nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene2Button.SetActive(true);
+        }
+		
+		//after choice 2b
+       else if (primeInt == 70){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 71){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt ==72){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+       else if (primeInt == 73){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+				//Go to next scene
+				nextButton.SetActive(false);
+                allowSpace = false;
+                NextScene3Button.SetActive(true);
+        }
+		
+
+		
       //Please do NOT delete this final bracket that ends the Next() function:
      }
 
 // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and SceneChanges)
         public void Choice1aFunct(){
                 Char1name.text = "Cadet Smeg";
-                Char1speech.text = " I mean, it wouldn’t hurt to look around. This is a pretty big field after all… ";
+                Char1speech.text = "I mean, it wouldn’t hurt to look around. This is a pretty big field after all… ";
                 Char2name.text = "";
                 Char2speech.text = "";
                 primeInt = 19;
@@ -282,21 +355,53 @@ public void Next(){
                 allowSpace = true;
         }
         public void Choice1bFunct(){
-			  	Choice1a.SetActive(false);
-				Choice1b.SetActive(false);
-                nextButton.SetActive(true);
-                allowSpace = true;
-                Char1name.text = "Cadet Smeg";
+			   Char1name.text = "Cadet Smeg";
                 Char1speech.text = "Off I go!";
                 Char2name.text = "";
                 Char2speech.text = "";
                 primeInt = 48;
+			  	Choice1a.SetActive(false);
+				Choice1b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        } 
+		
+		public void Choice2aFunct(){
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 59;
+                Choice2a.SetActive(false);
+                Choice2b.SetActive(false);
+                nextButton.SetActive(true);
+                allowSpace = true;
+        }
+		public void Choice2bFunct(){
+	            allowSpace = true;
+                Char1name.text = "Cadet Smeg";
+                Char1speech.text = "";
+                Char2name.text = "";
+                Char2speech.text = "";
+                primeInt = 69;
+			  	Choice2a.SetActive(false);
+				Choice2b.SetActive(false);
+                nextButton.SetActive(true);
+
         } 
 
+
+
+
+
+
         public void SceneChange1(){
-               SceneManager.LoadScene("Scene_USA");
+               SceneManager.LoadScene("End_Lose4_Area51");
         }
         public void SceneChange2(){
-                SceneManager.LoadScene("Scene_Giza");
+                SceneManager.LoadScene("End_Win2_Giza");
+        }
+		public void SceneChange3(){
+                SceneManager.LoadScene("End_Lose5_Earth");
         }
 }
